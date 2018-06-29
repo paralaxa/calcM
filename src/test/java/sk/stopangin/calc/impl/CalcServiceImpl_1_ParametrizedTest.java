bc14e83abfcbda1a41ec7b4c1567e06b3f32abbe
martin.cuchran <martin.cuchran@globallogic.com>
2018-06-29 02:10:34 +0200

    .

--- src/test/java/sk/stopangin/calc/impl/CalcServiceImpl_1_ParametrizedTest.java
+++ src/test/java/sk/stopangin/calc/impl/CalcServiceImpl_1_ParametrizedTest.java
@@ -30,7 +30,7 @@ public class CalcServiceImpl_1_ParametrizedTest {
                 {1.0, 1.0, 0.0}, {2.0, 1.0, 1.0}, {3.0, 2.0, 1.0}, {4.0, 3.0, 1.0}, {5.0, 5.0, 0.0}, {6.0, 8.0, -2.0}
         });
     }
-
+//test cf-118
 //test
     @Before
     public void setUp() throws Exception {
